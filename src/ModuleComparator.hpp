92acf0da69108f131c79047db2cf51b0cc72b092
Yaroslav <yarebenko@gmail.com>
2018-07-23 13:34:20 +0300

    s

--- src/ModuleComparator.hpp
+++ src/ModuleComparator.hpp
@@ -5,8 +5,24 @@
 #ifndef GROBNERBASIS_MODULECOMPARATOR_H
 #define GROBNERBASIS_MODULECOMPARATOR_H
 
-class ModuleComparator {
+#include "Comparator.hpp"
+#include "ModuleTerm.hpp"
 
+template <typename Pow>
+class ModuleComparator {
+private:
+    Comparator cmp;
+public:
+    explicit ModuleComparator(const Comparator<Pow>& _cmp): cmp(_cmp) {}
+    virtual bool operator()(const ModuleTerm& a, const ModuleTerm& b) const = 0;
 };
+
+class Top: public ModuleComparator {
+    using ModuleComparator;
+    bool operator()(const ModuleTerm& a, const ModuleTerm& b) {
+        if (a.coef
+    }
+};
+
 
 #endif //GROBNERBASIS_MODULECOMPARATOR_H
